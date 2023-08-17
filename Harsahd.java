7d34f0c7df25a57b303821f707df16c4fd2415d1
Harshad Jadhav <109645557+HarshadSJadhav@users.noreply.github.com>
2023-08-17 16:55:33 +0530

    Update and rename Pratham.java to Harsahd.java

--- Harsahd.java
+++ Harsahd.java
@@ -1,10 +1,10 @@
 package project1;
 
-public class Pratham {
+public class Harshad {
 
 	public static void main(String[] args) {
 		
-System.out.println("Pratham,");
+System.out.println("Harshad,");
 	}
 
 }
